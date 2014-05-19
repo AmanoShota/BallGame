3db15f8a0ccddb9f265856663d1d16bb223ec224
AmanoShota <amanoshota15@gmail.com>
2014-05-19 13:40:11 +0900

    ボールゲーム

---
+++ Assets/NewBehaviourScript1.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript1 : MonoBehaviour {
+	public bool is_landing;
+	
+	// Use this for initialization
+	void Start () {
+		is_landing = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (is_landing) {
+			if (Input.GetMouseButtonDown (0)) {
+				this.rigidbody.velocity = Vector3.up * 20.0f; 
+				is_landing = false;
+			}
+		}
+		
+		
+	}
+	void OnCollisionEnter( Collision collision ){
+		if (collision.gameObject.tag == "Floor") {
+			is_landing = true;
+		}
+	}
+	
+}
---
+++ Assets/NewBehaviourScript2.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	private int frame_count;
+	
+	// Use this for initialization
+	void Start () {
+		//Debug.Log ("Ball called");
+		//	frame_count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//frame_count++;
+		//if(frame_count == 30){
+		//	frame_count = 0;
+		//	Debug.Log ("Ball called yes");
+	}
+	
+	//		void OnCollisionEnter(Collision collision){
+	//		if(colliision.gameObject
+	
+	void OnBecameInvisible()
+	{
+		Destroy (this.gameObject);
+	}
+}
